810fa4517bf7b46bc07d237aa2f9f666a525081a
Durga <durgasanal07@gmail.com>
2026-03-18 20:49:09 +0530

    Update server configuration

--- server/index.js
+++ server/index.js
@@ -5,22 +5,22 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 app.use(cors())
-require("./db.js")
+require("./config/db.js")
 
 app.use("/uploads",express.static("uploads/"))
 
 //controllers evda
-const adminController = require("./controllers/admincontroller.js")
-app.use("/admin",adminController)
+const adminRoutes = require("./routes/adminRoutes.js")
+app.use("/admin",adminRoutes)
 
-const creatorController = require("./controllers/creatorcontroller.js")
-app.use("/creator",creatorController)
+const creatorRoutes = require("./routes/creatorRoutes.js")
+app.use("/creator",creatorRoutes)
 
-const collaboratorController = require("./controllers/collaboratorcontroller.js")
-app.use("/collaborator",collaboratorController)
+const collaboratorRoutes= require("./routes/collaboratorRoutes.js")
+app.use("/collaborator",collaboratorRoutes)
 
-const mentorController = require("./controllers/mentorcontroller.js")
-app.use("/mentor",mentorController)
+const mentorRoutes = require("./routes/mentorRoutes.js")
+app.use("/mentor",mentorRoutes)
 
 app.listen(8080, ()=>{
     console.log("server running at port no:8080")
